e131368f97f64602edd4594e6fd709a9f2e68894
Aldair503 <60680810+Aldair503@users.noreply.github.com>
2020-02-29 17:55:36 -0600

    Add files via upload

---
+++ Ejercicio 3-G1/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Ejercicio_3_G1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Inver = new System.Windows.Forms.Label();
+            this.Lab1 = new System.Windows.Forms.Label();
+            this.labe2 = new System.Windows.Forms.Label();
+            this.labe3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLim = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.TexBox1 = new System.Windows.Forms.TextBox();
+            this.TexBox2 = new System.Windows.Forms.TextBox();
+            this.TexBox3 = new System.Windows.Forms.TextBox();
+            this.labe5 = new System.Windows.Forms.Label();
+            this.Suma = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Texbox = new System.Windows.Forms.TextBox();
+            this.Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Inver
+            // 
+            this.Inver.AutoSize = true;
+            this.Inver.Location = new System.Drawing.Point(265, 73);
+            this.Inver.Name = "Inver";
+            this.Inver.Size = new System.Drawing.Size(71, 13);
+            this.Inver.TabIndex = 0;
+            this.Inver.Text = "Inversionistas";
+            // 
+            // Lab1
+            // 
+            this.Lab1.AutoSize = true;
+            this.Lab1.Location = new System.Drawing.Point(203, 106);
+            this.Lab1.Name = "Lab1";
+            this.Lab1.Size = new System.Drawing.Size(16, 13);
+            this.Lab1.TabIndex = 1;
+            this.Lab1.Text = "1:";
+            // 
+            // labe2
+            // 
+            this.labe2.AutoSize = true;
+            this.labe2.Location = new System.Drawing.Point(203, 140);
+            this.labe2.Name = "labe2";
+            this.labe2.Size = new System.Drawing.Size(16, 13);
+            this.labe2.TabIndex = 2;
+            this.labe2.Text = "2:";
+            // 
+            // labe3
+            // 
+            this.labe3.AutoSize = true;
+            this.labe3.Location = new System.Drawing.Point(203, 169);
+            this.labe3.Name = "labe3";
+            this.labe3.Size = new System.Drawing.Size(16, 13);
+            this.labe3.TabIndex = 3;
+            this.labe3.Text = "3:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(162, 290);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLim
+            // 
+            this.btnLim.Location = new System.Drawing.Point(268, 289);
+            this.btnLim.Name = "btnLim";
+            this.btnLim.Size = new System.Drawing.Size(75, 23);
+            this.btnLim.TabIndex = 5;
+            this.btnLim.Text = "Limpiar";
+            this.btnLim.UseVisualStyleBackColor = true;
+            this.btnLim.Click += new System.EventHandler(this.btnLim_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(379, 288);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // TexBox1
+            // 
+            this.TexBox1.Location = new System.Drawing.Point(256, 106);
+            this.TexBox1.Name = "TexBox1";
+            this.TexBox1.Size = new System.Drawing.Size(100, 20);
+            this.TexBox1.TabIndex = 7;
+            // 
+            // TexBox2
+            // 
+            this.TexBox2.Location = new System.Drawing.Point(256, 143);
+            this.TexBox2.Name = "TexBox2";
+            this.TexBox2.Size = new System.Drawing.Size(100, 20);
+            this.TexBox2.TabIndex = 8;
+            // 
+            // TexBox3
+            // 
+            this.TexBox3.Location = new System.Drawing.Point(256, 173);
+            this.TexBox3.Name = "TexBox3";
+            this.TexBox3.Size = new System.Drawing.Size(100, 20);
+            this.TexBox3.TabIndex = 9;
+            // 
+            // labe5
+            // 
+            this.labe5.AutoSize = true;
+            this.labe5.Location = new System.Drawing.Point(253, 211);
+            this.labe5.Name = "labe5";
+            this.labe5.Size = new System.Drawing.Size(91, 13);
+            this.labe5.TabIndex = 13;
+            this.labe5.Text = "Total de inversión";
+            // 
+            // Suma
+            // 
+            this.Suma.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.Suma.Location = new System.Drawing.Point(256, 239);
+            this.Suma.Name = "Suma";
+            this.Suma.Size = new System.Drawing.Size(100, 20);
+            this.Suma.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox2.Location = new System.Drawing.Point(379, 173);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox3.Location = new System.Drawing.Point(379, 140);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // Texbox
+            // 
+            this.Texbox.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.Texbox.Location = new System.Drawing.Point(379, 103);
+            this.Texbox.Name = "Texbox";
+            this.Texbox.Size = new System.Drawing.Size(100, 20);
+            this.Texbox.TabIndex = 17;
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Location = new System.Drawing.Point(397, 72);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(58, 13);
+            this.Label.TabIndex = 18;
+            this.Label.Text = "Porcentaje";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.Texbox);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Suma);
+            this.Controls.Add(this.labe5);
+            this.Controls.Add(this.TexBox3);
+            this.Controls.Add(this.TexBox2);
+            this.Controls.Add(this.TexBox1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLim);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.labe3);
+            this.Controls.Add(this.labe2);
+            this.Controls.Add(this.Lab1);
+            this.Controls.Add(this.Inver);
+            this.Name = "Form1";
+            this.Text = "Inversionistas ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Inver;
+        private System.Windows.Forms.Label Lab1;
+        private System.Windows.Forms.Label labe2;
+        private System.Windows.Forms.Label labe3;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLim;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox TexBox1;
+        private System.Windows.Forms.TextBox TexBox2;
+        private System.Windows.Forms.TextBox TexBox3;
+        private System.Windows.Forms.Label labe5;
+        private System.Windows.Forms.TextBox Suma;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox Texbox;
+        private System.Windows.Forms.Label Label;
+    }
+}
+
---
+++ Ejercicio 3-G1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_3_G1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double Inver1, Inver2, Inver3, labe5, Label;
+            Inver1 = Convert.ToDouble(TexBox1.Text);
+            Inver2 = Convert.ToDouble(TexBox2.Text);
+            Inver3 = Convert.ToDouble(TexBox3.Text);
+            labe5 = (Inver1 + Inver2 + Inver3);
+            Suma.Text = string.Format("{0:N1}", labe5);
+            Label = (Inver1 / 100);
+            Texbox.Text = string.Format("{0:N1}",Label);
+            Label = (Inver2 / 100);
+            textBox3.Text= string.Format("{0:N1}", Label);
+            Label = (Inver3 / 100);
+            textBox2.Text=string.Format("{0:N1}", Label);
+
+
+
+
+
+        }
+
+        private void Total1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnLim_Click(object sender, EventArgs e)
+        {
+            TexBox1.Clear();
+            TexBox2.Clear();
+            TexBox3.Clear();
+            Suma.Clear();
+            Texbox.Clear();
+            textBox3.Clear();
+            textBox2.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
