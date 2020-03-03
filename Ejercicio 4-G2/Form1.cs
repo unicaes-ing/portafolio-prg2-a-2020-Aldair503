e88a6168326fd209fa415f0ec1cd7d4954d8ac01
Aldair503 <60680810+Aldair503@users.noreply.github.com>
2020-03-02 20:16:22 -0600

    Add files via upload

---
+++ Ejercicio 4-G2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_4_G2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            ErrorProvider.Clear();
+        }
+
+        private void txtContraseña_TextChanged(object sender, EventArgs e)
+        {
+            ErrorProvider.Clear();
+        }
+       
+        }
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool validar = true;
+            ErrorProvider.Clear();
+            if (txtContraseña.Text == txtrecontra.Text)
+            {
+                validar = true;
+            }
+            else
+            {
+                validar = false;
+                ErrorProvider.SetError(txtrecontra, "La contraseña no son iguales");
+            }
+            if (validar)
+            {
+                MessageBox.Show("Los datos han sido guardados");
+            }
+        }
+    }
+}
