d2ccdaba65c526ffeb5875fa22d54eed6e447dbf
Aldair503 <60680810+Aldair503@users.noreply.github.com>
2020-03-02 18:17:36 -0600

    Add files via upload

---
+++ Ejercicio 2-G1/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Ejercicio_2_G1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCant = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrec = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdb1 = new System.Windows.Forms.RadioButton();
+            this.rdb2 = new System.Windows.Forms.RadioButton();
+            this.rdb3 = new System.Windows.Forms.RadioButton();
+            this.rdb4 = new System.Windows.Forms.RadioButton();
+            this.rdb5 = new System.Windows.Forms.RadioButton();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cantidad";
+            // 
+            // txtCant
+            // 
+            this.txtCant.Location = new System.Drawing.Point(137, 55);
+            this.txtCant.Name = "txtCant";
+            this.txtCant.Size = new System.Drawing.Size(100, 20);
+            this.txtCant.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Precio";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtPrec
+            // 
+            this.txtPrec.Location = new System.Drawing.Point(137, 93);
+            this.txtPrec.Name = "txtPrec";
+            this.txtPrec.Size = new System.Drawing.Size(100, 20);
+            this.txtPrec.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdb5);
+            this.groupBox1.Controls.Add(this.rdb4);
+            this.groupBox1.Controls.Add(this.rdb3);
+            this.groupBox1.Controls.Add(this.rdb2);
+            this.groupBox1.Controls.Add(this.rdb1);
+            this.groupBox1.Location = new System.Drawing.Point(279, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(123, 164);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Descuento";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // rdb1
+            // 
+            this.rdb1.AutoSize = true;
+            this.rdb1.Location = new System.Drawing.Point(33, 19);
+            this.rdb1.Name = "rdb1";
+            this.rdb1.Size = new System.Drawing.Size(39, 17);
+            this.rdb1.TabIndex = 0;
+            this.rdb1.TabStop = true;
+            this.rdb1.Text = "0%";
+            this.rdb1.UseVisualStyleBackColor = true;
+            // 
+            // rdb2
+            // 
+            this.rdb2.AutoSize = true;
+            this.rdb2.Location = new System.Drawing.Point(33, 42);
+            this.rdb2.Name = "rdb2";
+            this.rdb2.Size = new System.Drawing.Size(39, 17);
+            this.rdb2.TabIndex = 1;
+            this.rdb2.TabStop = true;
+            this.rdb2.Text = "5%";
+            this.rdb2.UseVisualStyleBackColor = true;
+            // 
+            // rdb3
+            // 
+            this.rdb3.AutoSize = true;
+            this.rdb3.Location = new System.Drawing.Point(33, 65);
+            this.rdb3.Name = "rdb3";
+            this.rdb3.Size = new System.Drawing.Size(45, 17);
+            this.rdb3.TabIndex = 2;
+            this.rdb3.TabStop = true;
+            this.rdb3.Text = "10%";
+            this.rdb3.UseVisualStyleBackColor = true;
+            // 
+            // rdb4
+            // 
+            this.rdb4.AutoSize = true;
+            this.rdb4.Location = new System.Drawing.Point(33, 90);
+            this.rdb4.Name = "rdb4";
+            this.rdb4.Size = new System.Drawing.Size(45, 17);
+            this.rdb4.TabIndex = 3;
+            this.rdb4.TabStop = true;
+            this.rdb4.Text = "15%";
+            this.rdb4.UseVisualStyleBackColor = true;
+            this.rdb4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // rdb5
+            // 
+            this.rdb5.AutoSize = true;
+            this.rdb5.Location = new System.Drawing.Point(33, 113);
+            this.rdb5.Name = "rdb5";
+            this.rdb5.Size = new System.Drawing.Size(45, 17);
+            this.rdb5.TabIndex = 4;
+            this.rdb5.TabStop = true;
+            this.rdb5.Text = "20%";
+            this.rdb5.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(151, 129);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Descuento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Total";
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(137, 172);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(100, 20);
+            this.txtDesc.TabIndex = 8;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(137, 207);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 9;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(62, 251);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 10;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(162, 251);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 11;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtPrec);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCant);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCant;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrec;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdb5;
+        private System.Windows.Forms.RadioButton rdb4;
+        private System.Windows.Forms.RadioButton rdb3;
+        private System.Windows.Forms.RadioButton rdb2;
+        private System.Windows.Forms.RadioButton rdb1;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Ejercicio 2-G1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_2_G1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int cant = Convert.ToInt32(txtCant.Text);
+            double prec = Convert.ToDouble(txtPrec.Text);
+            double n = 0.0;
+            try
+            {
+                if (rdb1.Checked==true)
+                {
+                    n= prec * cant*0.0;
+                }
+                else if (rdb2.Checked==true)
+                {
+                    n= prec *cant *0.5;
+                }
+                else if (rdb3.Checked==true)
+                {
+                    n = prec*cant *0.10;
+                }
+                else if (rdb4.Checked==true)
+                {
+                     n= prec*cant * 0.15;
+                }
+                else if (rdb5.Checked==true)
+                {
+                     n= prec*cant *0.20;
+                }
+                txtDesc.Text = n.ToString("N2");
+                txtTotal.Text =((cant*prec)).ToString("N2");
+
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Seleccione una cantidad de descuento");
+            }
+        }
+
+    }
+}
